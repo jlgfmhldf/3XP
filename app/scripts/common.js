ee34df7002077bbcae2de6ba7920d8fc20774d33
jlgfmhldf <jlgfmhldf@gmail.com>
2015-07-31 23:53:39 +0300

    feat(text): multilingualism

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -3,7 +3,8 @@ $(function () {
 	'use strict';
 
 	var
-		$scrollBtn = $('.js-scroll-to');
+		$scrollBtn = $('.js-scroll-to'),
+		$langBtn = $('.js-lang-btn');
 
 	$scrollBtn.on('click', function (event) {
 		var scrollId = $(this).attr('href');
@@ -15,6 +16,18 @@ $(function () {
 		});
 
 		event.preventDefault()
+	});
+
+	$langBtn.on('click', function () {
+		var
+			$this = $(this),
+			value = $this.attr('data-lang'),
+			activeClass = 'lang_state_active';
+
+		$('html').attr('lang', value);
+
+		$langBtn.removeClass(activeClass);
+		$this.addClass(activeClass);
 	});
 
 });
